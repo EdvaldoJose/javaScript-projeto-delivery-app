e5e377990d30dc09cd50562e02401a09411b10e7
Thiago <programadorthiagolopes@gmail.com>
2023-03-29 17:31:07 -0300

    feat: Add Redirect rota "/"

--- front-end/src/Routes/Route.jsx
+++ front-end/src/Routes/Route.jsx
@@ -1,12 +1,14 @@
-import { Route, Switch } from 'react-router-dom';
+import { Redirect, Route, Switch } from 'react-router-dom';
 import React from 'react';
 import Login from '../Pages/Login';
 
 function Rotas() {
   return (
     <Switch>
-      <Route exact path="/" component={ Login } />
-      <Route exact path="/login" component={ Login } />
+      <Route exact path="/">
+        <Redirect to="/login" />
+      </Route>
+      <Route path="/login" component={ Login } />
     </Switch>
   );
 }
