68e34756deddf4e52742fe9a39d31c97316be781
Thiago <programadorthiagolopes@gmail.com>
2023-03-29 14:22:46 -0300

    feat: Add disable

--- front-end/src/Components/Button/index.jsx
+++ front-end/src/Components/Button/index.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function Button({ onclick, name, dataTestId }) {
+function Button({ onclick, name, dataTestId, disabled }) {
   return (
     <button
       type="button"
       onClick={ onclick }
       data-testid={ dataTestId }
+      disabled={ disabled }
     >
       { name }
 
