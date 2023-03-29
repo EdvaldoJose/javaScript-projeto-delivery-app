1dec4734f7af661f638f74842b0d40de5bfe0282
Thiago <programadorthiagolopes@gmail.com>
2023-03-29 08:32:41 -0300

    feat: add rotas no app

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,15 +1,9 @@
 import React from 'react';
-import './App.css';
-import rockGlass from './images/rockGlass.svg';
+import Rotas from './Routes/Route';
 
 function App() {
   return (
-    <div className="App">
-      <span className="logo">TRYBE</span>
-      <object className="rocksGlass" type="image/svg+xml" data={ rockGlass }>
-        Glass
-      </object>
-    </div>
+    <Rotas />
   );
 }
 
