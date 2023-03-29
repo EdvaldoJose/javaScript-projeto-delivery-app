1c082a79e8d5eb19c4682d919326893cc258a907
Thiago <programadorthiagolopes@gmail.com>
2023-03-29 14:27:56 -0300

    feat: Reducer

--- front-end/src/Redux/reducers/user.js
+++ front-end/src/Redux/reducers/user.js
@@ -1,4 +1,11 @@
-import { USER_LOGIN } from '../actions';
+import {
+  USER_LOGIN_EMAIL,
+  USER_LOGIN_PASSWORD,
+  LOGIN_SUCESS,
+  LOGIN_FAILED,
+  IN_LOGIN,
+  USER_VALIDATE,
+} from '../actions';
 
 const initialState = {
   user: {
@@ -6,14 +13,65 @@ const initialState = {
     password: '',
     role: '',
   },
+  inLogin: {
+    allowed: false,
+    loggingIn: false,
+    disable: false,
+    message: '',
+    btnLogin: true,
+  },
 };
 
 const userReducer = (state = initialState, action) => {
   switch (action.type) {
-  case USER_LOGIN:
+  case USER_LOGIN_EMAIL:
     return {
       ...state,
-      user: { email: action.email, password: action.password },
+      user: { ...state.user, email: action.email },
+    };
+  case USER_LOGIN_PASSWORD:
+    return {
+      ...state,
+      user: { ...state.user, password: action.password },
+    };
+
+  case IN_LOGIN:
+    return {
+      ...state,
+      inLogin: { ...state.inLogin, loggingIn: state.inLogin.loggingIn },
+    };
+
+  case LOGIN_SUCESS:
+    return {
+      ...state,
+      inLogin: {
+        ...state.inLogin,
+        loggingIn: false,
+        allowed: true,
+        disable: false,
+      },
+      user: {
+        ...state,
+        role: action.data.role,
+      },
+    };
+
+  case LOGIN_FAILED:
+    return {
+      ...state,
+      inLogin: {
+        ...state.inLogin,
+        disable: true,
+        message: action.message,
+      },
+    };
+  case USER_VALIDATE:
+    return {
+      ...state,
+      inLogin: {
+        ...state,
+        btnLogin: false,
+      },
     };
 
   default:
