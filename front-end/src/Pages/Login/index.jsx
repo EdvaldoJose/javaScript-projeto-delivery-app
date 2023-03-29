898e7be5ecac1ef7a8c0e3a96debb68d2e4c6428
Thiago <programadorthiagolopes@gmail.com>
2023-03-29 09:59:25 -0300

    feat: Criado tela login (COMPONENTES)

--- front-end/src/Pages/Login/index.jsx
+++ front-end/src/Pages/Login/index.jsx
@@ -1,8 +1,45 @@
 import React from 'react';
+import Input from '../../Components/Input';
+import {
+  ROUTE,
+  ELEMENTEMAIL,
+  ELEMENTPASSWORD,
+  ELEMENTBTLOGIN,
+  ELEMENTBTREGISTER,
+  ELEMENTINVALIDEMAIL,
+} from '../../dataTesteIds';
+import Button from '../../Components/Button';
+import Invalid from '../../Components/Invalid';
 
 function Login() {
   return (
-    <div>Login</div>
+    <form>
+      <Input
+        inputName="email"
+        type="email"
+        name="input-email"
+        dataTesteId={ `${ROUTE}__${ELEMENTEMAIL}` }
+      />
+      <Input
+        inputName="password"
+        type="password"
+        name="input-password"
+        dataTesteId={ `${ROUTE}__${ELEMENTPASSWORD}` }
+      />
+      <Button
+        name="Login"
+        dataTesteId={ `${ROUTE}__${ELEMENTBTLOGIN}` }
+      />
+      <Button
+        name="Register"
+        dataTesteId={ `${ROUTE}__${ELEMENTBTREGISTER}` }
+      />
+      <Invalid
+        dataTestId={ `${ROUTE}__${ELEMENTINVALIDEMAIL}` }
+        message="Erro Login"
+        disabled={ false }
+      />
+    </form>
   );
 }
 
