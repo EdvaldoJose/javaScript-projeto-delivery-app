55ca0a1ee9beee85cacaea13099e68671629c1c1
Thiago <programadorthiagolopes@gmail.com>
2023-03-29 17:29:48 -0300

    fix: Controller

--- back-end/src/api/controllers/loginController.js
+++ back-end/src/api/controllers/loginController.js
@@ -1,13 +1,13 @@
 const service = require('../services/loginService');
 
 async function login(req, res) {
-  const { email } = req.body;
+  const { email, password } = req.body;
 
-  const { code, message, type, role } = await service.login(email);
+  const { code, message, type, role } = await service.login(email, password);
 
   if (type) return res.status(code).json({ message });
 
-  return res.status(code).json({ role });
+  return res.status(code).json(role);
 }
 
 module.exports = { login };
