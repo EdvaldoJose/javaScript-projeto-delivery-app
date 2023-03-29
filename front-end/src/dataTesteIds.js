63474af8ae45eaa07111fca9b30a314ab6c4a30d
Thiago <programadorthiagolopes@gmail.com>
2023-03-29 09:58:26 -0300

    feat: dataTesteIds

---
+++ front-end/src/dataTesteIds.js
@@ -0,0 +1,16 @@
+// Login
+const ROUTE = 'customer_products';
+const ELEMENTEMAIL = 'input-email';
+const ELEMENTPASSWORD = 'input-password';
+const ELEMENTBTLOGIN = 'button-login';
+const ELEMENTBTREGISTER = 'button-register';
+const ELEMENTINVALIDEMAIL = 'invalid-email';
+
+export {
+  ROUTE,
+  ELEMENTBTLOGIN,
+  ELEMENTBTREGISTER,
+  ELEMENTEMAIL,
+  ELEMENTINVALIDEMAIL,
+  ELEMENTPASSWORD,
+};
