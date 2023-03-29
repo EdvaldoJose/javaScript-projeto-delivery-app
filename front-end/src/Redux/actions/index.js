718bbcb6ab99be855413b33e0dbced6de08c20ab
Thiago <programadorthiagolopes@gmail.com>
2023-03-29 14:27:48 -0300

    feat: Actions

--- front-end/src/Redux/actions/index.js
+++ front-end/src/Redux/actions/index.js
@@ -1,3 +1,22 @@
-export const USER_LOGIN = 'USER_LOGIN';
+import axios from 'axios';
 
-export const userLogin = ({ email, password }) => ({ type: USER_LOGIN, email, password });
+export const USER_LOGIN_EMAIL = 'USER_LOGIN_EMAIL';
+export const USER_LOGIN_PASSWORD = 'USER_LOGIN_PASSWORD';
+export const IN_LOGIN = 'IN_LOGIN';
+export const LOGIN_SUCESS = 'LOGIN_SUCESS';
+export const LOGIN_FAILED = 'LOGIN_FAILED';
+export const USER_VALIDATE = 'USER_VALIDATE';
+
+export const userLoginEmail = (email) => ({ type: USER_LOGIN_EMAIL, email });
+export const userLoginPassword = (password) => ({ type: USER_LOGIN_PASSWORD, password });
+export const loggingIn = () => ({ type: IN_LOGIN });
+export const logginSucess = (data) => ({ type: LOGIN_SUCESS, data });
+export const logginFailed = ({ message }) => ({ type: LOGIN_FAILED, message });
+export const valideUser = () => ({ type: USER_VALIDATE });
+
+export const logar = ({ email, password }) => (dispatch) => {
+  dispatch(loggingIn());
+  axios.post('http://localhost:3001/login', { email, password })
+    .then((data) => dispatch(logginSucess(data.json())))
+    .catch((error) => dispatch(logginFailed(error)));
+};
