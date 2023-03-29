cea362081bf3508f89d8fcbe921f80433fd51702
Thiago <programadorthiagolopes@gmail.com>
2023-03-29 14:20:17 -0300

    feat:  Add Cors

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
+app.use(cors({ origin: '*' }));
+app.use(express.json());
 
 app.get('/coffee', (_req, res) => res.status(418).end());
 
