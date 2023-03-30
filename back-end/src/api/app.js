77d02e2df98637a9bd48568828b6387448272762
Thiago <programadorthiagolopes@gmail.com>
2023-03-30 11:50:32 -0300

    feat: Removido cors config

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const router = require('./routes');
 
 const app = express();
-app.use(cors({ origin: '*' }));
+app.use(cors());
 app.use(express.json());
 app.use(router);
 
