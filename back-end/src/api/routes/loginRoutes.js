8f89f93e9f213c84913b2b6ccde48c0e073485e0
Thiago <programadorthiagolopes@gmail.com>
2023-03-30 09:58:24 -0300

    feat: rota register

--- back-end/src/api/routes/loginRoutes.js
+++ back-end/src/api/routes/loginRoutes.js
@@ -4,5 +4,6 @@ const loginControllers = require('../controllers/loginController');
 const router = Router();
 
 router.post('/login', (req, res) => loginControllers.login(req, res));
+router.post('/register', (req, res) => loginControllers.createUser(req, res));
 
 module.exports = router;
