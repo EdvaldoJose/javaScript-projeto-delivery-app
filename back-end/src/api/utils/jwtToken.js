bea5bfe0915211318c42c9843fb6530bb5333795
Thiago <programadorthiagolopes@gmail.com>
2023-04-01 10:07:35 -0300

    feat: Reajustado caminhod o jwt token

--- back-end/src/api/utils/jwtToken.js
+++ back-end/src/api/utils/jwtToken.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 
 const createToken = (token) => {
   const secret = fs
-  .readFileSync(`${__dirname}/../../../../jwt.evaluation.key`, { encoding: 'utf-8' });
+  .readFileSync(`${__dirname}/../../../jwt.evaluation.key`, { encoding: 'utf-8' });
   return jwt.sign(token, secret);
 };
 
