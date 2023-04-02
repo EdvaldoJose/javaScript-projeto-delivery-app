a5119e45bce172e8a8d8e1af296221afe03dd4d7
Thiago <programadorthiagolopes@gmail.com>
2023-04-02 13:54:18 -0300

    feat: Novas Actions

--- front-end/src/Pages/Checkout/checkout.js
+++ front-end/src/Pages/Checkout/checkout.js
@@ -1,7 +1,13 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
-import React, { useState } from 'react';
+import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { atualizaItems } from '../../Redux/actions';
+import {
+  addEndereco,
+  addEnderecoNumber,
+  atualizaItems,
+  selectSeller,
+  addSubtotal,
+} from '../../Redux/actions';
 import NavBar from '../../Components/NavBar';
 import Tr from '../../Components/Tr';
 import {
@@ -22,33 +28,44 @@ import {
 
 export default function Checkout() {
   const { listProducts, total } = useSelector((state) => state.products);
-
-  const [setIdVendedor] = useState();
-
+  const { endereco, endNumber, sellers } = useSelector((state) => state.user);
   const dispatch = useDispatch();
 
-  const mockSalesman = [
-    {
-      id: 1,
-      nome: 'Fulana Pereira',
-    },
-  ];
+  const subtotal = () => {
+    const result = listProducts.reduce((acc, curr) => {
+      if (curr.quantity > 0) {
+        const totalItem = curr.price * curr.quantity;
+        acc += totalItem;
+      }
+      return acc;
+    }, 0);
+    const string = Number(
+      result.toString().replace(',', '.'),
+    ).toFixed(2).replace('.', ',');
+    dispatch(addSubtotal(string));
+  };
 
   const removeItem = (item) => {
     const itemFind = listProducts.find((bebida) => bebida.id === item.id);
-    // console.log(item);
-    console.log(itemFind);
     itemFind.quantity = 0;
     listProducts.splice(itemFind.id - 1, 1, itemFind);
+    localStorage.setItem('products', JSON.stringify(listProducts));
     dispatch(atualizaItems(listProducts));
+    subtotal();
+  };
+
+  const handleVendedor = ({ target: { value } }) => {
+    console.log(value);
+    dispatch(selectSeller(value));
   };
 
   const handleId = () => {
     // props.history.push(`/customer/orders/${idVendendor}`);
   };
 
-  const handleVendedor = ({ target }) => {
-    setIdVendedor(target.value);
+  const handleChange = ({ target: { value, name } }) => {
+    if (name === 'end') return dispatch(addEndereco(value));
+    dispatch(addEnderecoNumber(value));
   };
 
   return (
@@ -87,13 +104,14 @@ export default function Checkout() {
                 <td
                   data-testid={ `${CUSTOMERCHECKOUT}${ORDERUNITYPRICE}${index}` }
                 >
-                  {item.price}
+                  {item.price.replace(/\./, ',')}
 
                 </td>
                 <td
                   data-testid={ `${CUSTOMERCHECKOUT}${ORDERSUBTOTAL}${index}` }
                 >
-                  {item.price * item.quantity}
+                  {Number((item.price * item.quantity)
+                    .toString().replace(',', '.')).toFixed(2).replace('.', ',')}
 
                 </td>
                 <td
@@ -119,30 +137,40 @@ export default function Checkout() {
           <h3>Detalhes e Endereço da Entrega</h3>
           <label htmlFor="vendedora">
             P. Vendedora responsável:
+            <select
+              data-testid={ `${CUSTOMERCHECKOUT}${ORDERSALE}` }
+              id="vendedora"
+              onClick={ handleVendedor }
+            >
+              {sellers.map(
+                (p, index) => <option key={ index }>{p}</option>,
+              )}
+            </select>
           </label>
-          <select
-            data-testid={ `${CUSTOMERCHECKOUT}${ORDERSALE}` }
-            onClick={ (elemento) => handleVendedor(elemento) }
-            id="vendedora"
-          >
-            {mockSalesman.map(
-              (p) => <option key={ p.id } value={ p.id }>{p.nome}</option>,
-            )}
-          </select>
-          <label htmlFor="end">Endereço</label>
+          <label htmlFor="end">
+            Endereço
 
-          <input
-            data-testid={ `${CUSTOMERCHECKOUT}${INPUTADDRES}` }
-            id="end"
-            placeholder="Travessa Terceira da Castanheira, Bairro Muruci"
-          />
-          <label htmlFor="num">Número</label>
+            <input
+              onChange={ handleChange }
+              data-testid={ `${CUSTOMERCHECKOUT}${INPUTADDRES}` }
+              id="end"
+              name="end"
+              value={ endereco }
+            />
+          </label>
 
-          <input
-            data-testid={ `${CUSTOMERCHECKOUT}${INPUTNUMBER}` }
-            id="num"
-            placeholder="198"
-          />
+          <label htmlFor="num">
+            Número
+
+            <input
+              data-testid={ `${CUSTOMERCHECKOUT}${INPUTNUMBER}` }
+              id="num"
+              name="num"
+              onChange={ handleChange }
+              value={ endNumber }
+            />
+          </label>
+
           <button
             data-testid={ `${CUSTOMERCHECKOUT}${FINISHBUTTON}` }
             type="button"
