81f6249c2d30c882b68cc3cdedebc1594aec23c4
Thiago <programadorthiagolopes@gmail.com>
2023-04-03 11:58:20 -0300

    feat: Transaction

--- back-end/src/api/services/salesService.js
+++ back-end/src/api/services/salesService.js
@@ -1,4 +1,12 @@
-const { Sale, User } = require('../../database/models');
+const Sequelize = require('sequelize');
+const { Sale, User, SaleProduct } = require('../../database/models');
+require('dotenv').config();
+
+const environment = process.env.NODE_ENV || 'development';
+
+const config = require('../../database/config/config');
+
+const sequelize = new Sequelize(config[environment]);
 
 const getAllSellers = async () => {
   const sellers = await User.findAll({ where: { role: 'seller' } });
@@ -6,10 +14,23 @@ const getAllSellers = async () => {
   return sellers;
 };
 
-const createSale = async (body) => {
-  const result = await Sale.create(body);
-
-  return result;
+const createSale = async ({ 
+  sellerId, userId, totalPrice, deliveryAddress, deliveryNumber, status, productList }) => {
+    try {
+      const result = await sequelize.transaction(async (t) => {
+        const sale = await Sale.create(
+    { sellerId, userId, totalPrice, deliveryAddress, deliveryNumber, status, saleDate: new Date() },
+    { transaction: t },
+        );
+        const listProducts = productList.map((item) => 
+        ({ productId: item.id, quantity: item.quantity, saleId: sale.id }));
+        await SaleProduct.bulkCreate(listProducts, { transaction: t });
+        return sale;
+      });
+    return result;
+    } catch (error) {
+      console.log(error);
+    }
 };
 
 module.exports = { createSale, getAllSellers };
