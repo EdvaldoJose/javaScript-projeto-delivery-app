f77823086645ca61f74e5ec91f410cdea639459b
Thiago <programadorthiagolopes@gmail.com>
2023-04-03 19:06:43 -0300

    feat: GetProductBySale id

--- back-end/src/api/routes/salesRoutes.js
+++ back-end/src/api/routes/salesRoutes.js
@@ -6,5 +6,6 @@ const salesRouter = Router();
 salesRouter.post('/', (req, res) => salesController.createSales(req, res));
 salesRouter.get('/sellers', (req, res) => salesController.getAllSellers(req, res));
 salesRouter.get('/orders/:id', (req, res) => salesController.getAllSalesOrders(req, res));
+salesRouter.get('/:id', (req, res) => salesController.getProductsBySale(req, res));
 
 module.exports = salesRouter;
