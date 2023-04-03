9933df305d67d246db972a796f2a7c88d4e5468a
Thiago <programadorthiagolopes@gmail.com>
2023-04-03 12:49:10 -0300

    feat: render

--- front-end/src/Pages/SellerOrders/index.jsx
+++ front-end/src/Pages/SellerOrders/index.jsx
@@ -1,11 +1,50 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import NavBar from '../../Components/NavBar';
 import useSellerOrders from '../../utils/getSellerOrders';
 
 function SellerOrders() {
   useSellerOrders();
+  const { sellerOrders } = useSelector((state) => state.products);
   return (
-    <NavBar />
+    <>
+      <NavBar />
+      <div>
+        {
+          sellerOrders.map((item, index) => (
+            <div key={ index }>
+              <div
+                data-testid={ `seller_orders__element-order-id-${item.id}` }
+              >
+                { item.id }
+              </div>
+              <div>
+                <div
+                  data-testid={ `seller_orders__element-delivery-status-${item.id}` }
+                >
+                  {item.status}
+                </div>
+                <p
+                  data-testid={ `seller_orders__element-card-address-${item.id}` }
+                >
+                  {`${item.deliveryAddress}, ${item.deliveryNumber}`}
+                </p>
+                <p
+                  data-testid={ `seller_orders__element-card-price-${item.id}` }
+                >
+                  {(item.totalPrice).replace('.', ',')}
+                </p>
+                <p
+                  data-testid={ `seller_orders__element-order-date-${item.id}` }
+                >
+                  {item.saleDate}
+                </p>
+
+              </div>
+            </div>))
+        }
+      </div>
+    </>
   );
 }
 
