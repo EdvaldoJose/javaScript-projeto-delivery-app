79ad129bea0fced7e6f09adfbe19ee637402ba4b
Thiago <programadorthiagolopes@gmail.com>
2023-04-04 09:12:45 -0300

    feat: Route Put

--- back-end/src/api/routes/salesRoutes.js
+++ back-end/src/api/routes/salesRoutes.js
@@ -4,6 +4,7 @@ const salesController = require('../controllers/salesController');
 const salesRouter = Router();
 
 salesRouter.post('/', (req, res) => salesController.createSales(req, res));
+salesRouter.put('/:id', (req, res) => salesController.updateSaleStatus(req, res));
 salesRouter.get('/sellers', (req, res) => salesController.getAllSellers(req, res));
 salesRouter.get('/orders/:id', (req, res) => salesController.getAllSalesOrders(req, res));
 salesRouter.get('/:id', (req, res) => salesController.getProductsBySale(req, res));
