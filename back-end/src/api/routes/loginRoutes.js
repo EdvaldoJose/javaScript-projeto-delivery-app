0d85cc878775ef2e508739876408d5834ee0ea28
Thiago <programadorthiagolopes@gmail.com>
2023-04-04 12:23:18 -0300

    feat: Criado users route

--- back-end/src/api/routes/loginRoutes.js
+++ back-end/src/api/routes/loginRoutes.js
@@ -3,7 +3,8 @@ const loginControllers = require('../controllers/loginController');
 
 const router = Router();
 
-router.post('/', (req, res) => loginControllers.login(req, res));
+router.post('/register/adm', (req, res) => loginControllers.createUserByAdm(req, res));
 router.post('/register', (req, res) => loginControllers.createUser(req, res));
+router.post('/', (req, res) => loginControllers.login(req, res));
 
 module.exports = router;
