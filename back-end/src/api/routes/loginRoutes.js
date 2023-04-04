408bcdfcb6073580db5b5d47164d1912af55a8ab
Thiago <programadorthiagolopes@gmail.com>
2023-04-04 14:10:22 -0300

    feat: Route

--- back-end/src/api/routes/loginRoutes.js
+++ back-end/src/api/routes/loginRoutes.js
@@ -6,5 +6,6 @@ const router = Router();
 router.post('/register/adm', (req, res) => loginControllers.createUserByAdm(req, res));
 router.post('/register', (req, res) => loginControllers.createUser(req, res));
 router.post('/', (req, res) => loginControllers.login(req, res));
+router.get('/users', (req, res) => loginControllers.getAllUsers(req, res));
 
 module.exports = router;
