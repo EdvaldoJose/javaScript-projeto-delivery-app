1316997912e9a84e95adc2778bbe9c6bef357c8f
Thiago <programadorthiagolopes@gmail.com>
2023-04-04 10:50:23 -0300

    feat: State admin

---
+++ front-end/src/Redux/reducers/admin.js
@@ -0,0 +1,45 @@
+import {
+  NEW_USER_NAME,
+  NEW_USER_EMAIL,
+  NEW_USER_PASSWORD,
+  NEW_USER_ROLE,
+} from '../actions';
+
+const initialState = {
+  newUser: {
+    username: '',
+    email: '',
+    password: '',
+    role: 'customer',
+    btnDisable: true,
+  },
+};
+
+const adminReducer = (state = initialState, action) => {
+  switch (action.type) {
+  case NEW_USER_NAME:
+    return {
+      ...state,
+      newUser: { ...state.newUser, username: action.value },
+    };
+  case NEW_USER_EMAIL:
+    return {
+      ...state,
+      newUser: { ...state.newUser, email: action.value },
+    };
+  case NEW_USER_PASSWORD:
+    return {
+      ...state,
+      newUser: { ...state.newUser, password: action.value },
+    };
+  case NEW_USER_ROLE:
+    return {
+      ...state,
+      newUser: { ...state.newUser, role: action.value },
+    };
+  default:
+    return { ...state };
+  }
+};
+
+export default adminReducer;
