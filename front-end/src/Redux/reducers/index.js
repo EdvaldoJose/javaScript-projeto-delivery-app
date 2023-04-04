938bfa62d24f2ab446d772e5c6d9cce984ac18e8
Thiago <programadorthiagolopes@gmail.com>
2023-04-04 10:51:06 -0300

    feat: reducer

--- front-end/src/Redux/reducers/index.js
+++ front-end/src/Redux/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import loginReducer from './inLogin';
 import userReducer from './user';
 import productsReducer from './products';
+import adminReducer from './admin';
 
 const rootReducer = combineReducers({
   user: userReducer,
   products: productsReducer,
   inLogin: loginReducer,
+  admin: adminReducer,
 });
 
 export default rootReducer;
