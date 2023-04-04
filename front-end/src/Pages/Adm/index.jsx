3f5bf5f36884ab18247b4abb7f1de7fd66a29563
Thiago <programadorthiagolopes@gmail.com>
2023-04-04 10:50:03 -0300

    feat: Form Admin

--- front-end/src/Pages/Adm/index.jsx
+++ front-end/src/Pages/Adm/index.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import Form from '../../Components/Form';
 import NavBar from '../../Components/NavBar';
 
 function Adm() {
   return (
-    <NavBar />
+    <>
+      <NavBar />
+      <Form />
+    </>
   );
 }
 
